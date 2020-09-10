712c2f0e31a2a1ca93bfbf732d0ef331dafb8c9e
Ythlee <yc872027415@163.com>
2020-09-10 17:03:02 +0800

    format

--- led/led_s&c/leds_02/led.c
+++ led/led_s&c/leds_02/led.c
@@ -4,9 +4,9 @@
 void led_conf(unsigned int cfg)
 {
     if (cfg) { //LED点亮
-	GPBDAT &= ~(1 << 5);
+        GPBDAT &= ~(1 << 5);
     } else { //LED熄灭
-	GPBDAT |= (1 << 5);
+        GPBDAT |= (1 << 5);
     }
     delay(0xfe00);
 }
